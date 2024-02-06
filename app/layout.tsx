97bde848fc837f3fb0ebb8bfb91d25ca674b5c70
JOHN CARTER <mukulsingh2276@gmail.com>
2024-02-06 22:06:26 +0530

    added dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/redux/providers";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast"
+import { ThemeProvider } from "@/components/providers/theme-providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +20,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
-        <Toaster/>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Providers>
+            {children}
+          </Providers>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/commons/Navbar.tsx
+++ components/commons/Navbar.tsx
@@ -7,33 +7,35 @@ import { ShoppingBag } from "lucide-react"
 import { getCategories } from '@/redux/reducers/getCategories';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { useRouter } from 'next/navigation';
+import { Button } from '../ui/button';
+import { ThemeToggle } from './ThemeToggle';
 const Navbar = () => {
 
-    const dispatch = useAppDispatch();
-    const [isMounted, setIsMounted] = useState(false);
+  const dispatch = useAppDispatch();
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect( () => {
-      setIsMounted(true);
-      dispatch(getCategories());
-    },[]);
+  useEffect(() => {
+    setIsMounted(true);
+    dispatch(getCategories());
+  }, []);
 
-    const categories = useAppSelector( state => state.ecomm.categories);    
-    const cartProducts = useAppSelector( state => state.ecomm.cartProducts);
-    const router = useRouter();
+  const categories = useAppSelector(state => state.ecomm.categories);
+  const cartProducts = useAppSelector(state => state.ecomm.cartProducts);
+  const router = useRouter();
 
-    if( !isMounted) return null;
+  if (!isMounted) return null;
 
   return (
     <main className='p-4 h-20 border flex items-center gap-10 w-full'>
-        <Link
-            href={`/`} 
-            className='text-3xl font-bold'>
-            Store
-        </Link>
-        <section className='flex gap-5'>
+      <Link
+        href={`/`}
+        className='text-3xl font-bold'>
+        Store
+      </Link>
+      <section className='flex gap-5'>
         {
-          categories && categories?.map( (category:Icategory) => (
-            <Link 
+          categories && categories?.map((category: Icategory) => (
+            <Link
               href={`/category/${category.id}`}
               key={category.id}
               className='text-lg transition hover:scale-110  hover:underline underline-offset-8'>
@@ -41,19 +43,20 @@ const Navbar = () => {
             </Link>
           ))
         }
-        </section>
-
-          <button 
-            onClick={ () => router.push('/cart')}
-            className="flex 
-            gap-3 
-            p-3 
-            rounded-full 
-            ml-auto 
-            border">
-            <ShoppingBag/>
-              <h1 className='text-lg'>{cartProducts?.length}</h1>
-          </button>
+      </section>
+      <section className='flex items-center gap-5 ml-auto'>
+        <ThemeToggle />
+        <button
+          onClick={() => router.push('/cart')}
+          className="flex 
+              p-3
+              rounded-full 
+              ml-auto 
+              border">
+          <ShoppingBag className='mr-2' />
+          <h1 className='text-lg'>{cartProducts?.length}</h1>
+        </button>
+      </section>
     </main>
   )
 }
